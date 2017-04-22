c004659b3fb91bb73be57a672c018e86ea2558e3
paulvill <paul.villoutreix@gmail.com>
2017-04-22 15:37:52 -0400

    comments

--- ssl_estimate.m
+++ ssl_estimate.m
@@ -1,11 +1,29 @@
-function [inv_u] = ssl_estimate( W, D,indl, indu)
-%SSL_ESTIMATE Summary of this function goes here
-%   Detailed explanation goes here
-	
+%SSL_ESTIMATE Computes the matrix required to obtain the predicted sample
+%values on unlabeled data points from labeled data points
+%
+% Usage 
+%    [inv_u] = ssl_estimate( W, indl, indu)
+%
+% Input
+%     W: affinity matrix containing the pairwise difference w_i,j between
+%     all the data points
+%     indl: indices of the labeled points
+%     indu: indices of the unlabeled points
+% 
+% Output
+%     inv_u: it is the matrix necessary to compute the predicted labels
+%     from the unlabeled ones, f_u = inv_u*Y where Y correspond to the
+%     labels
+
+function [inv_u] = ssl_estimate( W, indl, indu)
 	n = length(indl)+length(indu);
     indtot = [indl,indu];
     l = length(indl);
 	u = n-l;
+    
+    % matrix transforming arbitrary distribution of labeled and unlabeled
+    % data points into the classical ordered distribution of (un)labeled
+    % data points, it simplifies the subsequent formulation of the solution
     T = zeros(length(indu)+length(indl));
     for i = 1:n
         for j = 1:n
@@ -14,6 +32,8 @@ function [inv_u] = ssl_estimate( W, D,indl, indu)
             end
         end
     end
+    
+    D = diag(sum(W, 2));
     
     D_j = T*D*(T');
     W_j = T*W*(T');
@@ -26,6 +46,7 @@ function [inv_u] = ssl_estimate( W, D,indl, indu)
 	Wul = Wlu';
 	Wuu = W_j(l+1:l+u,l+1:l+u);
     
+    % solution of the harmonic extension problem
     inv_u = (Du-Wuu)\Wul;
 
 end
