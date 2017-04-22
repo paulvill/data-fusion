d0f634a6291cd6e15468f0c6764492678b3d7cb2
paulvill <paul.villoutreix@gmail.com>
2017-04-22 15:11:15 -0400

    add references for subfolders

--- addpath_datafusion.m
+++ addpath_datafusion.m
@@ -1,8 +1,13 @@
+% scripts from Lim, B. et al., Current Biology, 2015
 addpath(fullfile(fileparts(mfilename('fullpath')), 'DLL15'));
+% scripts from Dsilva, C.J. et al., Development, 2015
 addpath(fullfile(fileparts(mfilename('fullpath')), 'LDL15'));
+% scripts from Lederman, R.R. and Talmon, R., Tech. Rep., Yale, 2014
 addpath(fullfile(fileparts(mfilename('fullpath')), 'LT14'));
 
+% ScatNet Toolbox for Scattering Transform
 addpath 'scatnet-0.2';
 addpath_scatnet;
 
+%Visualization settings
 presets;
