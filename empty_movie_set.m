7df2eb1d456c57e1375835986a334d45e8c287bf
Joakim Andén <janden@math.princeton.edu>
2017-04-25 17:16:06 -0400

    Add empty_movie_set
    Just to have a basic movie set that doesn't have any movies yet.
    This way, we can construt a set using cat_movie_sets.

---
+++ empty_movie_set.m
@@ -0,0 +1,30 @@
+% EMPTY_MOVIE_SET Construct an empty movie set
+%
+% Usage
+%    movie_set = empty_movie_set(npixels, channels);
+%
+% Input
+%    npixels: The width (and height) of the images in the (square) movies
+%       (default 100).
+%    channels: The number of channels in the movies (default 1).
+%
+% Output
+%    movie_set: An empty set of movies.
+
+function movie_set = empty_movie_set(npixels, channels)
+    if nargin < 1 || isempty(npixels)
+        npixels = 100;
+    end
+
+    if nargin < 2 || isempty(channels)
+        channels = 1;
+    end
+
+    movie_set = struct();
+
+    movie_set.images = zeros([npixels*ones(1, 2) channels 0]);
+    movie_set.times = zeros(0, 1);
+    movie_set.angles = zeros(0, 1);
+    movie_set.movie_idx = zeros(0, 1);
+    movie_set.names = {};
+end
